c11b614d951c63c50b4a2d19872d283af3efd5a9
Blagica Bozhinovska <blagica.bozhinovska@skopje.prospective.ch>
2025-03-30 23:29:39 +0200

    Added more env vars

--- config/appInfo.ts
+++ config/appInfo.ts
@@ -1,8 +1,8 @@
 export const appInfo = {
     // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
     appName: "Finbits",
-    apiDomain: "http://localhost:3000",
-    websiteDomain: "http://localhost:3000",
+    apiDomain: process.env.API_DOMAIN || "http://localhost:3000",
+    websiteDomain: process.env.WEBSITE_DOMAIN || "http://localhost:3000",
     apiBasePath: "/api/auth",
     websiteBasePath: "/login",
 };
