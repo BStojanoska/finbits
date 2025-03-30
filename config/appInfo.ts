9692206773d7612ce2e1b40ad66ccb81e9c62100
Blagica Bozhinovska <blagica.bozhinovska@skopje.prospective.ch>
2025-03-30 23:45:38 +0200

    Change env name

--- config/appInfo.ts
+++ config/appInfo.ts
@@ -1,8 +1,7 @@
 export const appInfo = {
-    // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
     appName: "Finbits",
-    apiDomain: process.env.API_DOMAIN || "http://localhost:3000",
-    websiteDomain: process.env.WEBSITE_DOMAIN || "http://localhost:3000",
+    apiDomain: process.env.NUXT_API_DOMAIN || "http://localhost:3000",
+    websiteDomain: process.env.NUXT_WEBSITE_DOMAIN || "http://localhost:3000",
     apiBasePath: "/api/auth",
     websiteBasePath: "/login",
 };
